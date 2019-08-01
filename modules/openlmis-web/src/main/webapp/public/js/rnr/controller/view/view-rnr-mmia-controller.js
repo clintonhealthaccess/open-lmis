08194a5a3bc7a4233a3452204338fcea464dbed1
wang xiu <xiu.wang@thoughtworks.com>
2019-08-01 11:07:38 +0800

    Fix UT.

--- modules/openlmis-web/src/main/webapp/public/js/rnr/controller/view/view-rnr-mmia-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/rnr/controller/view/view-rnr-mmia-controller.js
@@ -137,7 +137,7 @@ function ViewRnrMmiaController($scope, $route, Requisitions, messageService, dow
         };
         var invalideKey = _.keys(openlmisMessageMap);
         var patientQuantifications = _.filter($scope.rnr.patientQuantifications, function(value) {
-            return _.includes(invalideKey, value.category.toLocaleLowerCase());
+            return _.includes(invalideKey, value.category && value.category.toLocaleLowerCase());
         });
         patientQuantifications = _.map(patientQuantifications, function(value) {
             var category = openlmisMessageMap[value.category.toLocaleLowerCase()];
