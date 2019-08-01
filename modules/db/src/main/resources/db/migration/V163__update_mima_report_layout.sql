59a2c74a655b485f7353405304b2d3ad715f8c22
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2019-08-01 15:36:22 +0800

    [jinghui] #6 remove the program_regimen_columns for comunitaryPharmacy

--- modules/db/src/main/resources/db/migration/V163__update_mima_report_layout.sql
+++ modules/db/src/main/resources/db/migration/V163__update_mima_report_layout.sql
@@ -1,4 +1,2 @@
 ALTER TABLE regimen_line_items ADD COLUMN comunitaryPharmacy CHAR(255);
 ALTER TABLE patient_quantification_line_items ADD COLUMN tableName CHAR(255);
-INSERT INTO program_regimen_columns (programid,name,label,visible,datatype,modifiedby) VALUES (1,'comunitaryPharmacy','Comunitary pharmacy
-',true,'regimen.reporting.dataType.numeric',1);
