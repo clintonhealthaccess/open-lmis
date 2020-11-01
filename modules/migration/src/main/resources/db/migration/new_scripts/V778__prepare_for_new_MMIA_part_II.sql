99278c186180220b10538142e4e193905c58450d
Peng.Cao <pcao@thoughtworks.com>
2020-11-01 21:32:37 +0800

    [Trello No.247] New MMIA Layout: amend SQL file

--- modules/migration/src/main/resources/db/migration/new_scripts/V778__prepare_for_new_MMIA_part_II.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V778__prepare_for_new_MMIA_part_II.sql
@@ -12,13 +12,13 @@ $do$
             INSERT INTO program_products(versioncode,programid,productid,productcategoryid,dosespermonth,active,currentprice,createdby,createddate,modifiedby,modifieddate,displayorder,fullsupply,isacoefficientsid)
             SELECT '87',(SELECT id from programs WHERE code='MMIA'),(SELECT id from products WHERE code = '08S39Z'),(SELECT id from product_categories WHERE "name"='Adult'),pp.dosespermonth,true,pp.currentprice,pp.createdby,pp.createddate,pp.modifiedby,pp.modifieddate,pp.displayorder,pp.fullsupply,pp.isacoefficientsid
             FROM program_products pp JOIN products p ON pp.productid = p.id  JOIN product_categories pc ON pc.id = pp.productcategoryid  JOIN  programs p2 ON p2.id  = pp.programid
-            WHERE p.code='08S39Z';
+            WHERE p.code='08S39Z' AND p2.code='MMIA';
 
             --Children for '87'
             INSERT INTO program_products(versioncode,programid,productid,productcategoryid,dosespermonth,active,currentprice,createdby,createddate,modifiedby,modifieddate,displayorder,fullsupply,isacoefficientsid)
             SELECT '87',(SELECT id from programs WHERE code='MMIA'),(SELECT id from products WHERE code = '08S01Z'),(SELECT id from product_categories WHERE "name"='Children'),pp.dosespermonth,true,pp.currentprice,pp.createdby,pp.createddate,pp.modifiedby,pp.modifieddate,pp.displayorder,pp.fullsupply,pp.isacoefficientsid
             from program_products pp join products p on pp.productid = p.id  join product_categories pc on pc.id = pp.productcategoryid  join  programs p2 on p2.id  = pp.programid
-            WHERE p.code='08S01Z';
+            WHERE p.code='08S01Z' AND p2.code='MMIA';
 
         END IF;
     END
