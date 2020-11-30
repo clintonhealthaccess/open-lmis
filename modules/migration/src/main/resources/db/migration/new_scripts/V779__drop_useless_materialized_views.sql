461e52802af03998d57e8f10f92e82dea1a92b34
Jinghua Li <jinhli@thoughtworks.com>
2020-11-30 14:56:10 +0800

    [Trello No.275] Drop useless materialized views

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V779__drop_useless_materialized_views.sql
@@ -0,0 +1,4 @@
+DROP MATERIALIZED VIEW IF EXISTS public.vw_lot_expiry_dates;
+DROP MATERIALIZED VIEW IF EXISTS public.vw_weekly_nos_soh;
+DROP MATERIALIZED VIEW IF EXISTS public.vw_weekly_tracer_soh;
+DROP MATERIALIZED VIEW IF EXISTS public.vw_period_movements;
