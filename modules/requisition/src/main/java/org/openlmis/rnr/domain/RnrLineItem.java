02d75d0405136a89f1deab7a30ceef608474f220
fightwing <18071766136@163.com>
2021-03-10 09:56:23 +0800

    [Boyu Yuan] Remove manual filling verification restrictions for test in dev

--- modules/requisition/src/main/java/org/openlmis/rnr/domain/RnrLineItem.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/domain/RnrLineItem.java
@@ -197,7 +197,7 @@ public class RnrLineItem extends LineItem {
 
     for (String fieldName : nonNullableFields) {
       boolean nullOrNegative =
-          template.columnsVisible(fieldName) && !template.columnsCalculated(fieldName) && (
+          template.columnsVisible(fieldName) && (
               getValueFor(fieldName) == null || (Integer) getValueFor(fieldName) < 0);
       if (nullOrNegative) {
         LOGGER.error("facilityId {} programId {}, product code {} filed {} is {}",
